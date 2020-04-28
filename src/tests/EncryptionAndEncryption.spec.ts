726191c401dc04ae41eeebdafa17cc7a0fdd6ea0
Carlos Rombaldo Jr <jrrombaldo@users.noreply.github.com>
2020-04-28 16:51:49 +0100

    putting "\n" back

--- src/tests/EncryptionAndEncryption.spec.ts
+++ src/tests/EncryptionAndEncryption.spec.ts
@@ -58,7 +58,7 @@ describe("testing password and encryption together", () => {
     const passwordService: PasswordService = PasswordServiceFactory.create();
     returnedPassword = passwordService.searchForPassword(volume);
 
-    expect(returnedPassword.passwordValue).to.eql(passwordValue);
+    expect(returnedPassword.passwordValue).to.eql(passwordValue + "\n");
   });
 
   it("mount volume", () => {

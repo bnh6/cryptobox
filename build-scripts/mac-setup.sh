3f11684e54945834e46bf6654a259b3a218824f0
Carlos Rombaldo <junicarl@amazon.com>
2020-12-16 06:47:37 -0300

    fix encfs4win path && moving mac to script

---
+++ build-scripts/mac-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+csrutil status
+id
+#  sudo xcode-select --reset
+if (csrutil status | grep -isq " enabled"); then
+    echo "SIP is enabled, not installing CryFS nor EncFS"
+else
+    echo "SIP is disabled :)"
+    brew install --cask osxfuse
+    brew install cryfs
+fi

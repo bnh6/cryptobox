fc9d95c6973ac5bd88b4d4b237df7f81159ffbcc
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-12-26 05:42:27 -0300

    pwshl cmt

--- build-scripts/win-setup.cmd
+++ build-scripts/win-setup.cmd
@@ -6,12 +6,11 @@ choco feature disable -n=showDownloadProgress
 choco install vcredist2017
 choco install -y dokany
 
-<#
-curl.exe --output vc_redist.x64.exe --location --url https://aka.ms/vs/16/release/vc_redist.x64.exe
-Start-Process msiexec.exe -Wait -ArgumentList '/I vc_redist.x64.exe /quiet /qn /norestart /L*V “vc.log”’
-curl.exe --output dokan64.msi --location  https://github.com/dokan-dev/dokany/releases/latest/download/Dokan_x64.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I dokan64.msi /quiet /qn /norestart /L*V "dokan.log"'
-#>
+# curl.exe --output vc_redist.x64.exe --location --url https://aka.ms/vs/16/release/vc_redist.x64.exe
+# Start-Process msiexec.exe -Wait -ArgumentList '/I vc_redist.x64.exe /quiet /qn /norestart /L*V “vc.log”’
+# curl.exe --output dokan64.msi --location  https://github.com/dokan-dev/dokany/releases/latest/download/Dokan_x64.msi
+# Start-Process msiexec.exe -Wait -ArgumentList '/I dokan64.msi /quiet /qn /norestart /L*V "dokan.log"'
+
 
 curl.exe --output cryfs64.msi --location --url https://github.com/cryfs/cryfs/releases/download/0.10.2/cryfs-0.10.2-win64.msi
 start /wait msiexec.exe /I cryfs64.msi /quiet /qn /norestart /L*V "cryfs.log"

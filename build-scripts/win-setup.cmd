bdd7d5cec8876b5550861d4bf10a5a0f513e06ad
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-12-26 05:05:45 -0300

    win testing with ext again

--- build-scripts/win-setup.cmd
+++ build-scripts/win-setup.cmd
@@ -1,13 +1,14 @@
+
 choco feature disable -n=showDownloadProgress
 
 curl.exe --output vc_redist.x64.exe --location --url https://aka.ms/vs/16/release/vc_redist.x64.exe
-Start-Process msiexec.exe -Wait -ArgumentList '/I vc_redist.x64.exe /quiet /qn /norestart /L*V “vc.log”’
+call msiexec.exe /I vc_redist.x64.exe /quiet /qn /norestart /L*V “vc.log”
 
 curl.exe --output dokan64.msi --location  https://github.com/dokan-dev/dokany/releases/latest/download/Dokan_x64.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I dokan64.msi /quiet /qn /norestart /L*V "dokan.log"'
+call msiexec.exe /I dokan64.msi /quiet /qn /norestart /L*V "dokan.log"
 
 curl.exe --output cryfs64.msi --location --url https://github.com/cryfs/cryfs/releases/download/0.10.2/cryfs-0.10.2-win64.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I cryfs64.msi /quiet /qn /norestart /L*V "cryfs.log"'
+call msiexec.exe /I cryfs64.msi /quiet /qn /norestart /L*V "cryfs.log"
 
 echo "::group::Printing logs"
 type *.log

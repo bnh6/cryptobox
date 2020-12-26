a4fb607041f16bb707c3e5c44b8fe4db6d8defa2
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-12-26 19:15:55 -0300

    [wip] installation service tests

--- src/tests/DisableLogs.spec.ts
+++ src/tests/DisableLogs.spec.ts
@@ -1,5 +1,5 @@
 // disbaling logs for cleaner stdout (is this a good thing???)
 // for all services
 import log from "../services/LogService";
-log.transports.file.level = false;
-log.transports.console.level = false;
+// log.transports.file.level = false;
+// log.transports.console.level = false;
--- src/tests/InstallationService.spec.ts
+++ src/tests/InstallationService.spec.ts
@@ -1,13 +1,25 @@
 import { expect } from "chai";
 import * as os from "os";
 import * as installService from "../services/Installation/InstallationService";
+import * as shell from "../services/ShellService";
 
 describe("  >>>>  EXECUTING INSTALATION SERVICE  TESTS  <<<<  ", () => {
     it(`installing implementations on ${os.platform()}`, async () => {
         expect(async () => {
             installService.install();
         }).not.throw();
-    }).timeout(300000);
+    }).timeout(420000); //7min
+
+    it("encfs must be installed", async () => {
+        const [code, stdout, stderr] = await shell.execute("encfs", ["--version"], false);
+        expect(code).to.equal(0);
+    });
+
+    it("cryfs must be installed", async () => {
+        const [code, stdout, stderr] = await shell.execute("cryfs", ["--version"], false);
+        expect(code).to.equal(0);
+    });
+
     
 
     /**
